8d80445ffe56cc3050d2dc71f05619653b29d210
Mohamed Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2022-05-18 13:12:32 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,6 @@ def load_data():
     try : 
         DATA_URL = f'http://127.0.0.1:5000/{destination}'
         data = pd.read_csv('airbnb.csv')
-        data.drop(['_id'],axis=1,inplace=True)
         return data
     except Exception as ex : 
         st.exception(ex)
